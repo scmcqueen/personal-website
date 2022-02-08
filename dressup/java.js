5d2ca7dc810da8bab0b980756456c742c529482e
Skyeler McQueen <scm237@cornell.edu>
2022-02-07 21:59:24 -0800

    Created test dress up game

---
+++ dressup/java.js
@@ -0,0 +1,8 @@
+function onoff(id) {
+       var clothe = document.getElementById(id);
+       var d = document.getD
+       if(clothe.style.display == 'block')
+          clothe.style.display = 'none';
+       else
+          clothe.style.display = 'block';
+    }
