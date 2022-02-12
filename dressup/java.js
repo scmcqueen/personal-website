c05941a60c53a512d13a118eca1f31f8ec47932c
Skyeler McQueen <scm237@cornell.edu>
2022-02-11 18:00:29 -0800

    Dress Up Success!

--- dressup/java.js
+++ dressup/java.js
@@ -1,8 +1,38 @@
-function onoff(id) {
-       var clothe = document.getElementById(id);
-       var d = document.getD
-       if(clothe.style.display == 'block')
-          clothe.style.display = 'none';
-       else
-          clothe.style.display = 'block';
+function toggleonoff(id) {
+
+       var element = document.getElementById(id);
+       var parent = element.parentNode;
+       var number = parent.childNodes.length;
+
+       for (let i =0; i < number ; i++ ) {
+         let child = parent.childNodes.item(i);
+         // if it is equal to
+         console.log(child)
+         if (child.nodeType == 1){
+           if (child.id == id) {
+             child.style.display = 'block';
+           } else {
+             child.style.display = 'none';
+           }
+         }
+
+       }
+
+       // console.log(parent);
+       // console.log(parent.childNodes);
+
+
+      // parent.childNodes.forEach(onoff);
     }
+
+
+
+// function onoff(element) {
+//   console.log(element);
+//   if (element.nodeType == 1){
+//         if(element.style.display == 'block')
+//           element.style.display = 'none';
+//         else
+//           element.style.display = 'block';
+//         }
+//     }
