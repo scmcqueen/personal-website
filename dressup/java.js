a0fb4dcb86a693b6af5ce72f2cbdec6c05b73467
Skyeler McQueen <scm237@cornell.edu>
2022-02-11 18:04:18 -0800

    Formatting

--- dressup/java.js
+++ dressup/java.js
@@ -1,38 +1,22 @@
 function toggleonoff(id) {
 
-       var element = document.getElementById(id);
-       var parent = element.parentNode;
-       var number = parent.childNodes.length;
+  var element = document.getElementById(id);
+  var parent = element.parentNode;
+  var number = parent.childNodes.length;
 
-       for (let i =0; i < number ; i++ ) {
-         let child = parent.childNodes.item(i);
-         // if it is equal to
-         console.log(child)
-         if (child.nodeType == 1){
-           if (child.id == id) {
-             child.style.display = 'block';
-           } else {
-             child.style.display = 'none';
-           }
-         }
-
-       }
-
-       // console.log(parent);
-       // console.log(parent.childNodes);
-
-
-      // parent.childNodes.forEach(onoff);
+  for (let i = 0; i < number; i++) {
+    let child = parent.childNodes.item(i);
+    // if it is equal to
+    console.log(child)
+    if (child.nodeType == 1) {
+      if (child.id == id) {
+        child.style.display = 'block';
+      } else {
+        child.style.display = 'none';
+      }
     }
 
+  }
 
+}
 
-// function onoff(element) {
-//   console.log(element);
-//   if (element.nodeType == 1){
-//         if(element.style.display == 'block')
-//           element.style.display = 'none';
-//         else
-//           element.style.display = 'block';
-//         }
-//     }
